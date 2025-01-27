05d00519585cd08098e53eeae64f43e0ac4519b0
mdmasudrana271 <mr7123619@gmail.com>
2025-01-27 16:55:46 +0600

    add cors origin

--- blood_aid/settings.py
+++ blood_aid/settings.py
@@ -67,7 +67,9 @@ SRF_TRUSTED_ORIGINS = ['https://blood-aid-backend.onrender.com','https://*.127.0
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173", 
+    "http://127.0.0.1:5173",
+    'https://your-frontend-domain.com',
+    'https://blood-aid-backend.onrender.com', 
 ]
 
 
