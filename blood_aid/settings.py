370f98be5dfdcc2b2e7f980fdc51cf3a4ef7b8f9
mdmasudrana271 <mr7123619@gmail.com>
2025-01-27 14:09:10 +0600

    added trusted host url

--- blood_aid/settings.py
+++ blood_aid/settings.py
@@ -70,6 +70,15 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173", 
 ]
 
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+    'accept',
+    'origin',
+]
+
 ROOT_URLCONF = 'blood_aid.urls'
 
 TEMPLATES = [
